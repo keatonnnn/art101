fb67e8ab452925a69bb7be5cb967faf69484d092
keatonnnn <77092030+keatonnnn@users.noreply.github.com>
2021-01-26 14:08:31 -0800

    yub

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/**
+ * Author:    Keaton
+ * Created:   on tuesday
+ * Additional notes: n/a
+ * (c) Copyright  beato. gov
+ **/
+
+ //variable definitions and declarations
+ var make = "Mazda";
+ var model = "Bebop";
+ var color = "gray";
+ var year = 2014;
+
+
+var age = 2021 - year
+
+
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model:  " + model + "<br>");
+document.writeln("Color:  " + color + "<br>");
+document.writeln("Year:  " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
