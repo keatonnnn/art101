ab7fbfb40223b922ff5146f7e726d9f89ab8f874
keatonnnn <77092030+keatonnnn@users.noreply.github.com>
2021-02-17 12:00:07 -0800

    s
    s

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -8,8 +8,7 @@
 
 //DECLARE BUTTON VARS USING BUTTON TAG AND CLICK EVENT!!!
 
-//$("#challenges").toggleClass("special");
-
+//each of these vars also has a function that toggles the class of each div
 var buttonVar1 = $('<button>toggle me</button>').click(function () { $("#challenges").toggleClass("special"); });
 
 var buttonVar2 = $('<button>toggllee mee</button>').click(function () { $("#problems").toggleClass("special"); });
