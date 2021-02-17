a650af97dc806540819b8e42b1fa13bac391eb8e
keatonnnn <77092030+keatonnnn@users.noreply.github.com>
2021-02-17 12:25:55 -0800

    as
    s

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -15,9 +15,19 @@ var buttonVar2 = $('<button>toggllee mee</button>').click(function () { $("#prob
 
 var buttonVar3 = $('<button>toggle</button>').click(function () { $("#results").toggleClass("special"); });
 
+
 //APPEND EACH BUTTON TO ITS DIV!!!!
 $("#challenges").append(buttonVar1);
 
 $("#problems").append(buttonVar2);
 
 $("#results").append(buttonVar3);
+
+
+var buttonVar4 = $('<button id="bonus1">bonus button 1</button>').click(function(){ $("#bonus2").toggleClass("redbutton");
+});
+
+var buttonVar5 = $('<button id="bonus2">bonus button 2</button>').click(function(){ $("#bonus1").toggleClass("redbutton");
+});
+
+$("#output").append(buttonVar4, buttonVar5);
