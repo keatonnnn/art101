466bfc1b75ad72dd0b1c789069a437b4ee90ab60
Keaton Brasse <nbrasse@ucsc.edu>
2021-03-02 16:33:42 -0800

    m

--- lab15/js/lab15.js
+++ lab15/js/lab15.js
@@ -51,38 +51,39 @@
 
 
 
+$("#apiButton").click(function(){
+console.log("test");
 
 
+// Using the core $.ajax() method
+$.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://jsonplaceholder.typicode.com/todos/1",
+    // The data to send (will be converted to a query string)
+    data: {
+  "userId": 1,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+},
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
 
-function doApiShit(){
+    success: function(data) {
+        // do stuff
+        console.log(data);
 
-    console.log("Doing API Shit");}
- // Using the core $.ajax() method
-//  $.ajax({
-//      // The URL for the request (from the api docs)
-//      url: "https://xkcd.com/info.0.json",
-//      // The data to send (will be converted to a query string)
-//      data: {
-//              // here is where any data required by the api
-//              //   goes (check the api docs)
-//
-//            },
-//      // Whether this is a POST or GET request
-//      type: "GET",
-//      // The type of data we expect back
-//      dataType : "json",
-//      // What do we do when the api call is successful
-//      //   all the action goes in here
-//      success: function(data) {
-//          // do stuff
-//          console.log(data);
-//      },
-//      // What we do if the api call fails
-//      error: function (jqXHR, textStatus, errorThrown) {
-//          // do stuff
-//          console.log("Error:", textStatus, errorThrown);
-//      }
-//  })
-// }
+        $("#output").html(JSON.stringify(data));
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) {
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+        $("#output").html("Error");
+    }
+})
 
-$("#apibutton").click(doApiShit);
+});
